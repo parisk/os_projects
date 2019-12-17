09eb7ac0782136b81d8a554b3f1b9f1c51976ccd
MariosPitsali <marios.pitsali@gmail.com>
2019-12-17 17:06:53 +0000

    polished it

--- reading-files/sku_patterns.py
+++ reading-files/sku_patterns.py
@@ -2,27 +2,44 @@ import re
 import os
 
 SkuNumRegex = re.compile(r'(.*)_([0-9]{1,3}|[a-z]*).(jpg|jpeg|JPG|png|tif|psd)') #means SKU comes in form of 6 to 9 decimals, followed by a _ followed by 1 to 3 decimals - showing order - followed by image file extension
+
+
+def pure_sku(filename):
+    """prints the sku that corresponds to the image file, so everything before _"""
+    try:
+        result = SkuNumRegex.search(filename)
+        print ("SKU that corresponds to filename is: " + result.group(1) )
+    except AttributeError as e:
+        print (e, filename)
+
+def pure_order(filename, ret = False):
+    """prints the number that corresponds to the order the image has in the carousel in-site, so everything between _ and ."""
+    try:
+        result = SkuNumRegex.search(filename)
+        print ("Number that corresponds to carousel order is: " + result.group(2))
+        if ret:
+            return (result.group(2))
+    except AttributeError as e:
+        print (e, filename)
+
+def pure_extension(filename):
+    try:
+        result = SkuNumRegex.search(filename)
+        print ("Extension that corresponds to image type is: " + result.group(3))
+    except AttributeError as e:
+        print (e, filename)
+
 for (root,directories,files) in os.walk('.'):
     if "images" in root:
         for f in files:
             print ("-"*10 + "\n" + "File is: " + f)
             try:
-                result = SkuNumRegex.search(f)
-                print ("*" * 10)
-                print ("Match is: " + result.group())
-                print ("*" * 10)
-                print ("SKU that corresponds to filename is: " + result.group(1) )
-                print ("SKU that corresponds to carousel order is: " + result.group(2))
-                print ("SKU that corresponds to image type is: " + result.group(3))
+                pure_sku(f)
+                pure_order(f)
+                pure_extension(f)
             except AttributeError:
                 print (f + ": this one didn't match")
-        
-result = SkuNumRegex.search("345678-APRIL-BUND_12.jpeg")
+print ("---Walking through directory over---")
 
-def pure_sku (filename):
-    try:
-        result = SkuNumRegex.search(filename)
-        pass
-    except AttributeError:
-        pass
-    
+
+
