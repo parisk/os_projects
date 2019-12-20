fe61146e22dd4357f098dee6dd3c0f29b18a7d7a
MariosPitsali <marios.pitsali@gmail.com>
2019-12-20 15:18:46 +0000

    some images created successfully, still OSErrors coming up

--- reading-files/image_resize.py
+++ reading-files/image_resize.py
@@ -4,10 +4,11 @@ from sku_patterns import pure_sku, pure_order, pure_extension
 from resizeimage import resizeimage
 import re
 
-image_list =[]
 
+#changes directory to image folder
 os.chdir('images')
 
+#create list of pictures in directory
 image_list = [item.name for item in os.scandir('.') if item.is_file()]
 
 print (image_list)
@@ -15,16 +16,23 @@ print (image_list)
 for image in image_list:
     img = Image.open(image)
     width, height = img.size
+    #call on sku_patterns methods to determine sku,order, and extension
+    #image must be renamed so that it is +1 higher on order
     sku = pure_sku(image)
     order = int(pure_order(image)) +1
     extension = pure_extension(image)
+    
+    #create new_name
     new_name = sku + "_" + str(order) + "." + extension
+    
     if width >500 or height>500:
         try:
             print ("Now resizing file: " + image +"...")
             cover = resizeimage.resize_contain(img, [300,300])
             cover.save(new_name, img.format)
         except OSError as e:
-            print ("Error occuren at image :" + image , e)
+            print ("Error occured resizing image :" + image , e)
         finally:
             print ("*" * 10)
+    else:
+        print ("----Image does not exceed 500px in any dimension----")
